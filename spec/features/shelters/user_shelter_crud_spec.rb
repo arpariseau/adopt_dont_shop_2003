c19817c0f3377407f9e119b049a12d9ddeecccb5
Alex Pariseau <arpariseau@gmail.com>
2020-05-05 23:32:25 -0600

    Show individual shelter page - not working yet, string interpolation?

---
+++ spec/features/shelters/user_shelter_crud_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Shelter read page", type: :feature do
+  it "User shown shelter info on page visit" do
+    shelter_1 = Shelter.create(name: "Dumb Friends League",
+                               address: "2080 S. Quebec St.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80231")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+  end
+end
