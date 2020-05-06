03d53a27cfef27bb9a9b574f88a8b40197d3e4d5
Alex Pariseau <arpariseau@gmail.com>
2020-05-05 21:33:27 -0600

    Visit to shelter index page

---
+++ spec/features/shelters/user_shelter_index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Shelter index page", type: :feature do
+  it "User sees all shelters on index visit" do
+    shelter_1 = Shelter.create(name: "Dumb Friends League",
+                               address: "2080 S. Quebec St.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80231")
+
+    shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+                               address: "1241 W. Bayaud Ave.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80223")
+
+    visit "/shelters"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+  end
+end
