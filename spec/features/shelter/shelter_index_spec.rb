1e26558b5037c40b1bacd83b9fdd5cb1ee3243d2
Alex Pariseau <arpariseau@gmail.com>
2020-05-09 22:20:29 -0600

    Can now edit or delete shelters from index page

--- spec/features/shelter/shelter_index_spec.rb
+++ spec/features/shelter/shelter_index_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Shelter index page", type: :feature do
-  it "shows user all shelters on index visit" do
-    shelter_1 = Shelter.create(name: "Dumb Friends League",
+  before :each do
+    @shelter_1 = Shelter.create(name: "Dumb Friends League",
                                address: "2080 S. Quebec St.",
                                city: "Denver",
                                state: "CO",
                                zip: "80231")
 
-    shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+    @shelter_2 = Shelter.create(name: "Denver Animal Shelter",
                                address: "1241 W. Bayaud Ave.",
                                city: "Denver",
                                state: "CO",
                                zip: "80223")
 
     visit "/shelters"
-
-    expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content(shelter_2.name)
   end
+
+  it "shows user all shelters on index visit" do
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
+  end
+
+  it "has an edit link for each shelter on the index" do
+    click_button "Edit #{@shelter_1.name}"
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+  end
+
+  it "has a delete link for each shelter on the index" do
+    click_button "Delete #{@shelter_2.name}"
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to_not have_content(@shelter_2.name)
+  end
+
 end
